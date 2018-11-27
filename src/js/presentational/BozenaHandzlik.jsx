760953ef54bc48974385143d46a8d64748e6433d
Jakub Kozlik <kozlik.jakub@gmail.com>
2018-11-27 02:23:11 +0100

    remove bio from announcemment and fix typo

--- src/js/presentational/BozenaHandzlik.jsx
+++ src/js/presentational/BozenaHandzlik.jsx
@@ -1,72 +1,5 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
 
-// class BozenaHandzlik extends Component{
-//     constructor(props){
-//         super(props)
-//     }
-
-//     render(){
-//         return (
-//             <li className="announcementRoot">
-//                 <h2 className="announcementTitle">Otwarcie gabinetu endokrynologicznego od grudnia 2018</h2>
-//                 <article className="announcementContent">
-//                     <section className="overviewSection">
-//                         <div className="textContainer">
-//                             <div className="personData">
-//                                 <h3>dr Bożena Handzlik:</h3>
-//                                 <ul className="specializationList">
-//                                     <li>specjalista endokrynolog</li>
-//                                     <li>specjalista chorób wewnętrznych</li>
-//                                 </ul>
-//                             </div>
-//                             <div className="additionalInfo">
-//                                 <p>
-//                                     Absolwentka Śląskiej Akademii Medycznej w Katowicach w 1989. Asystent w Oddziale Chorób Wewnętrznych
-//                                     w Suchej Beskidzkiej oraz w Poradni Endokrynologicznej tamtejszegoo szpitala.
-//                                 </p>
-//                                 <div className="appointementInfo">
-//                                     <p>Przyjęcia odbywać się w poniedziałki, dwa razy w miesiącu.</p>
-//                                     <p>Wizyty tylko prywatne.</p>
-//                                     <p>Koszt: 110 zł, w tym USG tarczycy.</p>
-//                                 </div>
-//                             </div>
-                            
-//                         </div>
-//                         <div className='imageContainer'>
-//                             {/* <img alt="Endokrynologia" title="Gabinet Endokrynologiczny"/> */}
-//                             <img src="./dist/img/endokrynologia.jpg" alt="Endokrynologia" title="Gabinet Endokrynologiczny"/>
-//                         </div>
-//                     </section>
-//                     <section className="servicesSection">
-//                         <h3>Zakres działalności:</h3>
-//                         <ul>
-//                             <li>
-//                                 leczenie chorób:
-//                                 <ul>
-//                                     <li>tarczycy</li>
-//                                     <li>przysadki mózgowej</li>
-//                                     <li>nadnerczy</li>
-//                                     <li>jajników</li>
-//                                     <li>jąder</li>
-//                                 </ul>
-//                             </li>
-//                             <li>diagnostyka i leczenie otyłości</li>
-//                             <li>prowadzenie kobiet z chorobami endokrynologicznymi w ciąży i po porodzie</li>
-//                             <li>wstępna diagnostyka zaburzeń płodności i miesiączkowania</li>
-//                             <li>diagnostyka trądziku i hirsutyzmu</li>
-//                             <li>USG tarczycy</li>
-//                             <li>również diagnostyka zaburzeń endokrynologicznych u dzieci</li>
-//                         </ul>
-//                         <p>Chętnych zapraszamy do rejestracji na I-szym piętrze</p>
-//                     </section>
-//                 </article>
-//                 {/* <iframe src="http://www.srw-baratin.eu/ekran/all" allow="fullscreen"></iframe>  */}
-//             </li>
-//         )
-//     }
-
-// }
 
 const BozenaHandzlik = (props) =>  {
 
@@ -85,12 +18,8 @@ const BozenaHandzlik = (props) =>  {
                             </ul>
                         </div>
                         <div className="additionalInfo">
-                            <p>
-                                Absolwentka Śląskiej Akademii Medycznej w Katowicach w 1989. Asystent w Oddziale Chorób Wewnętrznych
-                                w Suchej Beskidzkiej oraz w Poradni Endokrynologicznej tamtejszegoo szpitala.
-                            </p>
                             <div className="appointementInfo">
-                                <p>Przyjęcia odbywać się w poniedziałki, dwa razy w miesiącu.</p>
+                                <p>Przyjęcia odbywać się będą w poniedziałki, dwa razy w miesiącu.</p>
                                 <p>Wizyty tylko prywatne.</p>
                                 <p>Koszt: 110 zł, w tym USG tarczycy.</p>
                             </div>
@@ -111,8 +40,7 @@ const BozenaHandzlik = (props) =>  {
                                 <li>tarczycy</li>
                                 <li>przysadki mózgowej</li>
                                 <li>nadnerczy</li>
-                                <li>jajników</li>
-                                <li>jąder</li>
+                                <li>jajników i jąder</li>
                             </ul>
                         </li>
                         <li>diagnostyka i leczenie otyłości</li>
