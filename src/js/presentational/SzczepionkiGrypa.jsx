140f044ce1b12e8cf4910c246c0a733b24d8f534
Jakub Kozlik <kozlik.jakub@gmail.com>
2019-09-18 03:20:01 +0200

    added free vaccine program info

--- src/js/presentational/SzczepionkiGrypa.jsx
+++ src/js/presentational/SzczepionkiGrypa.jsx
@@ -9,9 +9,9 @@ const SzczepionkiGrypa = (props) => {
                 <div className="imageContainer">
                     <img src="./dist/img/vaccine.jpg" alt="Szczepionka" title="Szczepionka"/>
                 </div>
-                <strong>UWAGA! UWAGA!</strong>
+                <strong id="title">UWAGA! UWAGA!</strong>
                 <p className="vaccineText">
-                    Zbliża się sezon grypowy. Przypominamy, że najskuteczniejszą profilaktyką przeciw grypie są szczepienia (zwłaszcza u osób starszych). 
+                    Zbliża się sezon grypowy. Informujemy, że wznowiono <strong>PROGRAM DARMOWYCH SZCZEPIEŃ PRZECIW GRYPIE</strong> dla osób powyżej 65 roku życia (dla pierwszych 300 zgłoszeń).
                 </p>
             </article>
         </div>
