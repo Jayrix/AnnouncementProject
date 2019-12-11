3b396424a06950eae587f268aee8a56dc39911d7
Jakub Kozlik <kozlik.jakub@gmail.com>
2019-12-11 13:09:11 +0100

    turned off Fibroscan

--- src/js/presentational/AnnouncementList.jsx
+++ src/js/presentational/AnnouncementList.jsx
@@ -7,9 +7,6 @@ import Geers from "./Geers.jsx";
 import CR from "./CR.jsx";
 import Podomedis from "./Podomedis.jsx";
 import Medea from "./Medea.jsx";
-import Fibroscan1 from "./Fibroscan/Fibroscan1.jsx";
-import Fibroscan2 from "./Fibroscan/Fibroscan2.jsx";
-import Fibroscan3 from "./Fibroscan/Fibroscan3.jsx";
 
 
 //zmienne konfiguracyjne sliding w lewo
@@ -42,9 +39,6 @@ class AnnouncementList extends Component{
 
         this.state = {
             announcements : [
-                <Fibroscan1/>,
-                <Fibroscan2/>,
-                <Fibroscan3/>,
                 <Medea/>,
                 <SzczepionkiGrypa/>,
                 <Podomedis/>,
