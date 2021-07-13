fdd3bcfa2c1463c5c24129c0eb4a748837eaf6e8
Jakub Kozlik <kozlik.jakub@gmail.com>
2021-07-13 12:09:45 +0200

    remove SzczepionkiGrypa CITO covid version

--- src/js/presentational/AnnouncementList.jsx
+++ src/js/presentational/AnnouncementList.jsx
@@ -7,7 +7,6 @@ import Medea from "./Medea.jsx";
 import CovidInfo from "./CovidInfo.jsx";
 import SzczepieniaCovid from "./SzczepieniaCovid.jsx";
 import Antiviral from "./Antiviral.jsx";
-import SzczepionkiGrypa from "./SzczepionkiGrypa.jsx";
 
 
 //zmienne konfiguracyjne sliding w lewo
@@ -40,15 +39,12 @@ class AnnouncementList extends Component{
 
         this.state = {
             announcements : [
-                <SzczepionkiGrypa/>,
                 <Antiviral/>,
                 <Medea/>,
                 <SzczepieniaCovid/>,
-                <SzczepionkiGrypa/>,
                 <CovidInfo/>,
                 <Podomedis/>,
                 <CR />,
-                <SzczepionkiGrypa/>,
                 ...geers_array,
                 <CovidInfo/>,
                 ...psp_array
