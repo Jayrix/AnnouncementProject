b4ec1475edbe842a49540419b20fb295ec0da15d
Jakub Kozlik <kozlik.jakub@gmail.com>
2024-08-26 00:43:41 +0200

    add TrzeciaSzczepionka.jsx

--- src/js/presentational/AnnouncementList.jsx
+++ src/js/presentational/AnnouncementList.jsx
@@ -11,7 +11,7 @@ import DrFiedor from "./DrFiedor.jsx";
 import Profilaktyka40plus from "./Profilaktyka40plus.jsx";
 import Recepty from "./Recepty.jsx";
 //import TestNaPrzeciwciala from "./TestNaPrzeciwciala.jsx";
-//import TrzeciaSzczepionka from "./TrzeciaSzczepionka.jsx";
+import TrzeciaSzczepionka from "./TrzeciaSzczepionka.jsx";
 //import Maseczki from "./Maseczki.jsx";
 //import WstrzymanieSzczepien from "./WstrzymanieSzczepien.jsx";
 //import ProgramSzczepien from "./ProgramSzczepien.jsx";
@@ -65,6 +65,7 @@ class AnnouncementList extends Component{
 
         this.state = {
             announcements : [
+                <TrzeciaSzczepionka/>,
                 <Ktomalek/>,
                 <HPV/>,
                 <ZleceniaRecept/>,
@@ -75,6 +76,7 @@ class AnnouncementList extends Component{
                 <Kardiolog/>,
                 <PracowniaUSG/>,
                 <Recepty/>,
+                <TrzeciaSzczepionka/>,
                 <Profilaktyka40plus/>,
                 <Antiviral/>,
                 <ZleceniaRecept/>,
--- src/js/presentational/Osteoporoza.jsx
+++ src/js/presentational/Osteoporoza.jsx
@@ -7,10 +7,10 @@ const Osteoporoza = (props) => {
             <article className="osteoContent">
                 <p>
                     Zapraszamy na <strong>badanie osteoporozy</strong> z odcinka lędźwiowego kręgosłupa lub z szyjki kości udowej, w OSTEOBUSIE na naszym parkingu <span id="timeInfo">w dniu 
-                    23.10.2023, od godziny 9:00 do 13:00</span>.
+                    24.09.2024</span>.
                 </p>
                 <p>
-                    Zapisy w rejestracji osobiście lub <span id="contactInfo">pod numerem 33 851 40 23.</span> Koszt badania 45 zł.
+                    Zapisy w rejestracji osobiście lub <span id="contactInfo">pod numerem 33 851 40 23.</span>
                 </p>
             </article>
         </div>
