425c689dd2e811573abc090afa7bc064ab5cb15f
Jakub Kozlik <kozlik.jakub@gmail.com>
2025-04-13 23:22:02 +0200

    modify ZleceniaRecept.jsx

--- src/js/presentational/ZleceniaRecept.jsx
+++ src/js/presentational/ZleceniaRecept.jsx
@@ -8,6 +8,7 @@ const ZleceniaRecept = (props) => {
             <article className="zleceniaReceptContent">
                 <p className="zleceniaReceptText">
                     Uprzejmie informujemy, że <strong>zlecenia na recepty realizowane są do 3 dni roboczych.</strong>
+                    &nbsp;Informujemy też, że leki refundowane mają te same ceny we wszystkich aptekach.
                 </p>
             </article>
         </div>
